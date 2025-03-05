7c068ed08a9f1a29c7700b6d2131c5a60c295d5b
jdelcastillo <jdelcastillo@ikerlan.es>
2025-03-05 17:36:48 +0100

    Update code repository

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,15 @@
-pytest test/ -m "only" \
+pytest tests/ -m "only" \
   -s \
   -vv \
   --cov=. \
   --cov-branch \
   --disable-warnings \
   -o junit_family=xunit1 \
-  --junitxml=test/junit_xml/xunit-result-00.xml \
+  --junitxml=tests/junit_xml/xunit-result-00.xml \
   --cov-report=term \
-  --cov-report=xml:test/cov_xml/coverage.xml \
-  --cov-report=html:test/cov_html \
-  --cov-config=test/.coveragerc \
-  --ignore=test/test_empty.py \
+  --cov-report=xml:tests/cov_xml/coverage.xml \
+  --cov-report=html:tests/cov_html \
+  --cov-config=tests/.coveragerc \
+  --ignore=tests/test_empty.py \
   --cov-fail-under=85
 
